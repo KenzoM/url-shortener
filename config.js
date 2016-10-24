28300ef30617075bff25da73d6e3975decd2302d
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-23 22:41:40 -0700

    typo in config file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'mongodb://heroku_0xrn515v:v09cpj5t25qtm73klv2poj5n5h@ds031157.mlab.com:31157';
+config.webhost = 'mongodb://heroku_0xrn515v:v09cpj5t25qtm73klv2poj5n5h@ds031157.mlab.com:31157/';
 // your MongoDB host and database name
 module.exports = config;
